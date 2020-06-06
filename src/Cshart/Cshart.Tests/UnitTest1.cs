33857b4a3415e5480f9167753f68e3ee09b2a134
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-06-06 18:54:26 +0200

    add solution skeleton

---
+++ src/Cshart/Cshart.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Cshart.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            _ = new Class1();
+        }
+    }
+}
---
+++ src/Cshart/Cshart/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cshart
+{
+    public class Class1
+    {
+    }
+}
