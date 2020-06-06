ed8421df242c7f92ca74035108374e164ee4500c
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-06-06 20:30:32 +0200

    add `Chart.Expand()`

--- src/Cshart/Cshart.Tests/UnitTest1.cs
+++ src/Cshart/Cshart.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using FluentAssertions;
 using Xunit;
 
 namespace Cshart.Tests
@@ -8,7 +9,19 @@ namespace Cshart.Tests
         [Fact]
         public void Test1()
         {
-            _ = new Class1();
+            Chart.Expand<UnitTest1>(x => x.Test1())
+                .Should().Be(@"
+System.Type.GetTypeFromHandle(System.RuntimeTypeHandle handle);
+System.Linq.Expressions.Expression.Parameter(System.Type type, System.String name);
+Cshart.Tests.UnitTest1.Test1();
+System.Reflection.MethodBase.GetMethodFromHandle(System.RuntimeMethodHandle handle);
+System.Array.Empty();
+System.Linq.Expressions.Expression.Call(System.Linq.Expressions.Expression instance, System.Reflection.MethodInfo method, System.Linq.Expressions.Expression[] arguments);
+System.Linq.Expressions.Expression.Lambda(System.Linq.Expressions.Expression body, System.Linq.Expressions.ParameterExpression[] parameters);
+Cshart.Chart.Expand(System.Linq.Expressions.Expression`1[[System.Action`1[[Cshart.Tests.UnitTest1, Cshart.Tests, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null]], System.Private.CoreLib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e]] selector);
+FluentAssertions.AssertionExtensions.Should(System.String actualValue);
+System.Array.Empty();
+FluentAssertions.Primitives.StringAssertions.Be(System.String expected, System.String because, System.Object[] becauseArgs);");
         }
     }
 }
---
+++ src/Cshart/Cshart/Chart.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Reflection;
+
+namespace Cshart
+{
+    public class Chart
+    {
+        public static string Expand<T>(Expression<Action<T>> selector)
+        {
+            var result = string.Empty;
+
+            var expression = (MethodCallExpression)selector.Body;
+            string name = expression.Method.Name;
+
+            var methodBase = typeof(T).GetMethod(name);
+            foreach (var instruction in methodBase.GetInstructions())
+            {
+                var methodInfo = instruction.Operand as MethodInfo;
+
+                if (methodInfo != null)
+                {
+                    var declaringType = methodInfo.DeclaringType;
+                    var parameters = methodInfo.GetParameters();
+
+                    result +=
+                        Environment.NewLine
+                        + string.Format(
+                            "{0}.{1}({2});",
+                            declaringType.FullName,
+                            methodInfo.Name,
+                            string.Join(
+                                ", ",
+                                parameters
+                                .Select(p => p.ParameterType.FullName + " " + p.Name)
+                                .ToArray()));
+                }
+            }
+
+            return result;
+        }
+    }
+}
