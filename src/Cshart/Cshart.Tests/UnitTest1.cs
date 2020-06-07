8f27c78c1114076fa953ae3d5ac512bfaf236925
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-06-07 17:57:42 +0200

    remove low-value assertion

--- src/Cshart/Cshart.Tests/UnitTest1.cs
+++ src/Cshart/Cshart.Tests/UnitTest1.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Diagnostics;
 using System.IO;
-using FluentAssertions;
 using Xunit;
 
 namespace Cshart.Tests
@@ -12,33 +10,6 @@ namespace Cshart.Tests
         public void Test1()
         {
             var dotGraph = Chart.Expand<UnitTest1>(x => x.Test1());
-
-            // NOTE: it is really annoying to test this way
-            dotGraph.Should().StartWith(
-                @"digraph ""UnitTest1.Test1()"" { " +
-                @"""Test1""[label=""Test1"",shape=ellipse]; " +
-                @"""GetTypeFromHandle""[label=""GetTypeFromHandle"",shape=ellipse]; " +
-                @"""Test1"" -> ""GetTypeFromHandle""[arrowhead=normal]; " +
-                @"""Parameter""[label=""Parameter"",shape=ellipse]; " +
-                @"""Test1"" -> ""Parameter""[arrowhead=normal]; " +
-                @"""Test1""[label=""Test1"",shape=ellipse]; " +
-                @"""Test1"" -> ""Test1""[arrowhead=normal]; " +
-                @"""GetMethodFromHandle""[label=""GetMethodFromHandle"",shape=ellipse]; " +
-                @"""Test1"" -> ""GetMethodFromHandle""[arrowhead=normal]; " +
-                @"""Empty""[label=""Empty"",shape=ellipse]; " +
-                @"""Test1"" -> ""Empty""[arrowhead=normal]; " +
-                @"""Call""[label=""Call"",shape=ellipse]; " +
-                @"""Test1"" -> ""Call""[arrowhead=normal]; " +
-                @"""Lambda""[label=""Lambda"",shape=ellipse]; " +
-                @"""Test1"" -> ""Lambda""[arrowhead=normal]; " +
-                @"""Expand""[label=""Expand"",shape=ellipse]; " +
-                @"""Test1"" -> ""Expand""[arrowhead=normal]; " +
-                @"""Should""[label=""Should"",shape=ellipse]; " +
-                @"""Test1"" -> ""Should""[arrowhead=normal]; " +
-                @"""Empty""[label=""Empty"",shape=ellipse]; " +
-                @"""Test1"" -> ""Empty""[arrowhead=normal]; " +
-                @"""StartWith""[label=""StartWith"",shape=ellipse]; " +
-                @"""Test1"" -> ""StartWith""[arrowhead=normal]; ");
 
             // NOTE: it could be useful to introduce tests,
             // which compare the current emitted PNG
@@ -52,7 +23,7 @@ namespace Cshart.Tests
                     CreateNoWindow = true,
                     UseShellExecute = false,
                     FileName = "dot",
-                    Arguments = $@"-T png -o {fileName+ ".png"} ""{path}""",
+                    Arguments = $@"-T png -o {fileName + ".png"} ""{path}""",
                 };
             Process.Start(pci).WaitForExit();
         }
