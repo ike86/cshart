ad0dee7bbef68ba1f8ec18e93a9299f1ce7302c1
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-08-02 15:03:13 +0200

    ChartBuilder_ AddTypes_adds_the_specified_types

---
+++ src/Cshart/Cshart.Tests/Chart_.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Xunit;
+
+namespace Cshart.Tests
+{
+    public class ChartBuilder_
+    {
+        [Theory, AutoData]
+        public void AddTypes_adds_the_specified_types(
+            ChartBuilder builder,
+            IEnumerable<Type> types)
+        {
+            types = types.ToArray();
+
+            builder.AddTypes(types);
+
+            builder.Types.Should().BeEquivalentTo(types);
+        }
+    }
+}
---
+++ src/Cshart/Cshart/ChartBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cshart.Tests
+{
+    public class ChartBuilder
+    {
+        private readonly List<Type> types = new List<Type>();
+
+        public IEnumerable<Type> Types => types;
+
+        internal void AddTypes(IEnumerable<Type> types)
+        {
+            this.types.AddRange(types);
+        }
+    }
+}
