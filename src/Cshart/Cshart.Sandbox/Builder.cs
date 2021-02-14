225e43586a6aef5532b5bcd9bf8a77d8611784aa
Pfeff Ákos György <ike86@users.noreply.github.com>
2021-02-14 18:37:02 +0100

    add edges representing composition

--- src/Cshart/Cshart.Sandbox/Builder.cs
+++ src/Cshart/Cshart.Sandbox/Builder.cs
@@ -1,7 +1,11 @@
 ﻿#nullable enable
 using System;
+using System.IO;
+using System.Linq;
 using DotNetGraph;
 using DotNetGraph.Attributes;
+using DotNetGraph.Core;
+using DotNetGraph.Edge;
 using DotNetGraph.Node;
 using DotNetGraph.SubGraph;
 
@@ -14,6 +18,8 @@ namespace Cshart.Sandbox
             var assemblyGraph = new DotSubGraph(assemblyName);
             AddTypes(types, assemblyGraph);
 
+            AddEdges(types, assemblyGraph);
+            
             var dotGraph = new DotGraph("foo");
             dotGraph.Elements.Add(assemblyGraph);
             return dotGraph;
@@ -25,6 +31,39 @@ namespace Cshart.Sandbox
             {
                 assemblyGraph.Elements.Add(
                     new DotNode(type.FullName) {Shape = new DotNodeShapeAttribute()});
+            }
+        }
+
+        private static void AddEdges(Type[] types, DotSubGraph assemblyGraph)
+        {
+            foreach (var type in types)
+            {
+                var typeNode = assemblyGraph.Elements.FirstOrDefault(
+                    e => e is DotNode n
+                         && n.Identifier == type.FullName);
+                var fields = type.GetFields();
+                foreach (var field in fields)
+                {
+                    if (TryGetTypeNode(assemblyGraph, () => field.FieldType) is { } fieldTypeNode)
+                    {
+                        assemblyGraph.Elements.Add(new DotEdge(typeNode, fieldTypeNode));
+                    }
+                }
+            }
+        }
+
+        private static IDotElement? TryGetTypeNode(DotSubGraph assemblyGraph, Func<Type> getType)
+        {
+            try
+            {
+                return assemblyGraph.Elements.FirstOrDefault(
+                    e => e is DotNode n
+                         && n.Identifier == getType().FullName);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+                return null;
             }
         }
     }
